94a0ccf39102120ce36c2a2e9b2ccb0fd9d1d187
we-BAK <hailemichaelkidist@gmail.com>
2026-08-18 17:20:13 +0300

    LoginViewModel

---
+++ View Model/Auth/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BMS.Models.Auth
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username or Email is required.")]
+        [Display(Name = "Username or Email")]
+        public string Identity { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        public string FingerPrint { get; set; } = string.Empty;
+        public string TimeZone { get; set; } = string.Empty;
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
